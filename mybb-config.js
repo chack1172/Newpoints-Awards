a55c01582c973f8a31a948c806414d00e8f9253e
chack1172 <nberardozzi@gmail.com>
2022-01-24 02:27:13 +0100

    Removed undefined function

--- mybb-config.js
+++ mybb-config.js
@@ -6,7 +6,6 @@ const url = `http://${domain}:8000`;
 
 // Check for settings file existance
 const stats = fs.statSync(settingsPath);
-writeSettings();
 fs.appendFile(
     settingsPath,
     `\n\$settings['bburl'] = \"${url}\";\n\$settings['cookiedomain'] = \".${domain}\";\n\$settings['cookiepath'] = \"\";\n`,
