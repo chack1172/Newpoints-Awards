12451376bed8253788d8aa02428df8c5176f7323
chack1172 <nberardozzi@gmail.com>
2022-01-24 02:15:39 +0100

    Check settings path before overriding

--- mybb-config.js
+++ mybb-config.js
@@ -3,6 +3,10 @@ const fs = require('fs');
 const settingsPath = "mybb/inc/settings.php";
 const domain = "localhost";
 const url = `http://${domain}:8000`;
+
+// Check for settings file existance
+const stats = fs.statSync(settingsPath);
+writeSettings();
 fs.appendFile(
     settingsPath,
     `\n\$settings['bburl'] = \"${url}\";\n\$settings['cookiedomain'] = \".${domain}\";\n\$settings['cookiepath'] = \"\";\n`,
